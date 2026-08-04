92a8f00b04a77b6c3f594f2f49c5a4449f70a022
hy-systems <info@hysystems.com.au>
2026-08-04 23:04:55 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,23 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        white: '#ffffff',
+        accent: '#dbaf56',
+        overlay: '#262626',
+      },
+      fontFamily: {
+        sans: ['Sarabun', 'sans-serif'],
+        serif: ['Roboto Condensed', 'serif'],
+        mono: ['Lexend', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}

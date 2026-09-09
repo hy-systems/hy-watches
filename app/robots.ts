383a27b435d86bac359d3cb23651124729a4f04e
infohywatches-code <info.hywatches@gmail.com>
2026-09-09 12:24:46 +1000

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,4 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://hysystems.com.au/sitemap.xml",
+    sitemap: "https://hywatches.com.au/sitemap.xml",
   };
 }

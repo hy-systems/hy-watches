62ab362986af963c110659266f4477c3bbb2179c
infohywatches-code <info.hywatches@gmail.com>
2026-09-10 13:20:57 +1000

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { unimaticLight: "#F6F6F6", unimaticAccent: "#AFFF00", unimaticDark: "#1C1C1C" },
+      fontFamily: { sans: ["Helvetica Neue", "Arial", "sans-serif"], mono: ["JetBrains Mono", "monospace"] }
+    },
   },
   plugins: [],
 };
